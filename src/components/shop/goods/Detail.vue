3f96400388d845d30de238401b3780b16acc4e31
米粒儿 <m13533226787@163.com>
2018-03-07 10:36:07 +0800

    商品详情和右侧栏数据渲染，放大镜效果

--- src/components/shop/goods/Detail.vue
+++ src/components/shop/goods/Detail.vue
@@ -1,13 +1,275 @@
 <template>
     <div>
         商品细节部分
+        <!-- 导航栏 -->
+        <div class="section">
+            <div class="location">
+                <span>当前位置：</span>
+                <a href="/index.html">首页</a> &gt;
+                <a href="/goods.html">购物商城</a>
+                <a href="/goods/42/1.html">商品详情</a>
+
+            </div>
+        </div>
+
+        <!-- 商品详情 -->
+        <div class="section">
+            <div class="wrapper clearfix">
+                <div class="wrap-box">
+                    <!--页面左边-->
+                    <div class="left-925">
+                        <div class="goods-box clearfix">
+                            <!--商品图片-->
+                            <div class="pic-box">
+                                <div class="magnifier" id="magnifier1">
+                                    <div class="magnifier-container">
+                                        <div class="images-cover"></div>
+                                        <!--当前图片显示容器-->
+                                        <div class="move-view"></div>
+                                        <!--跟随鼠标移动的盒子-->
+                                    </div>
+                                    <div class="magnifier-assembly">
+                                        <div class="magnifier-btn">
+                                            <span class="magnifier-btn-left">&lt;</span>
+                                            <span class="magnifier-btn-right">&gt;</span>
+                                        </div>
+                                        <!--按钮组-->
+                                        <div class="magnifier-line">
+                                            <ul class="clearfix animation03">
+                                                <li v-for="item in goodsDetail.imglist" :key="item.id">
+                                                    <div class="small-img">
+                                                        <img :src="item.original_path" alt="">
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <!--缩略图-->
+                                    </div>
+                                    <div class="magnifier-view"></div>
+                                    <!--经过放大的图片显示容器-->
+                                </div>
+                            </div>
+                            <!--/商品图片-->
+
+                            <!--商品信息-->
+                            <div class="goods-spec">
+                                <h1>{{goodsDetail.goodsinfo.title}}</h1>
+                                <p class="subtitle">{{goodsDetail.goodsinfo.zhaiyao}}</p>
+                                <div class="spec-box">
+                                    <dl>
+                                        <dt>货号</dt>
+                                        <dd id="commodityGoodsNo">{{goodsDetail.goodsinfo.goods_no}}</dd>
+                                    </dl>
+                                    <dl>
+                                        <dt>市场价</dt>
+                                        <dd>
+                                            <s id="commodityMarketPrice">¥{{goodsDetail.goodsinfo.market_price}}.00</s>
+                                        </dd>
+                                    </dl>
+                                    <dl>
+                                        <dt>销售价</dt>
+                                        <dd>
+                                            <em class="price" id="commoditySellPrice">¥{{goodsDetail.goodsinfo.sell_price}}.00</em>
+                                        </dd>
+                                    </dl>
+                                </div>
+
+                                <div class="spec-box">
+                                    <dl>
+                                        <dt>购买数量</dt>
+                                        <dd>
+                                            <div class="stock-box">
+                                                <input id="commodityChannelId" type="hidden" value="2">
+                                                <input id="commodityArticleId" type="hidden" value="98">
+                                                <input id="commodityGoodsId" type="hidden" value="0">
+                                                <input id="commoditySelectNum" type="text" maxlength="9" value="1" maxvalue="10" onkeydown="return checkNumber(event);">
+                                                <a class="add" onclick="addCartNum(1);">+</a>
+                                                <a class="remove" onclick="addCartNum(-1);">-</a>
+                                            </div>
+                                            <span class="stock-txt">
+                                                库存
+                                                <em id="commodityStockNum">{{goodsDetail.goodsinfo.stock_quantity}}</em>件
+                                            </span>
+                                        </dd>
+                                    </dl>
+                                    <dl>
+                                        <dd>
+                                            <div class="btn-buy" id="buyButton">
+                                                <button class="buy" onclick="cartAdd(this,'/',1,'/shopping.html');">立即购买</button>
+                                                <button class="add" onclick="cartAdd(this,'/',0,'/cart.html');">加入购物车</button>
+                                            </div>
+                                        </dd>
+                                    </dl>
+                                </div>
+
+                            </div>
+                            <!--/商品信息-->
+                        </div>
+
+                        <div id="goodsTabs" class="goods-tab bg-wrap">
+                            <!--选项卡-->
+                            <div id="tabHead" class="tab-head" style="position: static; top: 517px; width: 925px;">
+                                <ul>
+                                    <li>
+                                        <a class="selected" href="javascript:;">商品介绍</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;" class="">商品评论</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!--/选项卡-->
+
+                            <!--选项内容-->
+                            <div class="tab-content entry" style="display:block;">
+                                内容
+                            </div>
+
+                            <div class="tab-content" style="display: block;">
+                                <!--网友评论-->
+                                <div class="comment-box">
+                                    <!--取得评论总数-->
+                                    <form id="commentForm" name="commentForm" class="form-box" url="/tools/submit_ajax.ashx?action=comment_add&amp;channel_id=2&amp;article_id=98">
+                                        <div class="avatar-box">
+                                            <i class="iconfont icon-user-full"></i>
+                                        </div>
+                                        <div class="conn-box">
+                                            <div class="editor">
+                                                <textarea id="txtContent" name="txtContent" sucmsg=" " datatype="*10-1000" nullmsg="请填写评论内容！"></textarea>
+                                                <span class="Validform_checktip"></span>
+                                            </div>
+                                            <div class="subcon">
+                                                <input id="btnSubmit" name="submit" type="submit" value="提交评论" class="submit">
+                                                <span class="Validform_checktip"></span>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <ul id="commentList" class="list-box">
+                                        <p style="margin:5px 0 15px 69px;line-height:42px;text-align:center;border:1px solid #f7f7f7;">暂无评论，快来抢沙发吧！</p>
+                                        <li>
+                                            <div class="avatar-box">
+                                                <i class="iconfont icon-user-full"></i>
+                                            </div>
+                                            <div class="inner-box">
+                                                <div class="info">
+                                                    <span>匿名用户</span>
+                                                    <span>2017/10/23 14:58:59</span>
+                                                </div>
+                                                <p>testtesttest</p>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="avatar-box">
+                                                <i class="iconfont icon-user-full"></i>
+                                            </div>
+                                            <div class="inner-box">
+                                                <div class="info">
+                                                    <span>匿名用户</span>
+                                                    <span>2017/10/23 14:59:36</span>
+                                                </div>
+                                                <p>很清晰调动单很清晰调动单</p>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                    <!--放置页码-->
+                                    <div class="page-box" style="margin:5px 0 0 62px">
+                                        <div id="pagination" class="digg">
+                                            <span class="disabled">« 上一页</span>
+                                            <span class="current">1</span>
+                                            <span class="disabled">下一页 »</span>
+                                        </div>
+                                    </div>
+                                    <!--/放置页码-->
+                                </div>
+
+                                <!--/网友评论-->
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <!--/页面左边-->
+
+                    <!--页面右边-->
+                    <app-aside :list="goodsDetail.hotgoodslist"></app-aside>
+                    <!--/页面右边-->
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+//引入公共右侧边栏-商品列表 
+import AppAside from './subcom/CommonAside.vue'
+
+//导入插件的css与js以及jquery
+import '@/lib/imgzoom/css/magnifier.css'
+import '@/lib/imgzoom/js/magnifier.js'
+import $ from 'jquery'
+
+export default {
+    components: {
+        AppAside
+    },
+    data() {
+        return {
+            //拿到首页页面点击商品跳转的id
+            id: this.$route.params.id,
+            //购物车初始数量
+            num: 1,
+            goodsDetail: {
+                goodsinfo: {},
+                imglist: [],
+                hotgoodslist: []
+            }
+
+
+        }
+    },
+    methods: {
+        //根据当前id获取数据
+        getGoodsDetail() {
+            this.$http.get(this.$api.goodsDetail + this.id).then(res => {
+                if (res.data.status == 0) {
+                    //将数据赋给data中的goodsDetail
+                    this.goodsDetail = res.data.message;
+                }
+            })
+        }
+    },
+
+    created() {
+        this.getGoodsDetail();
+    },
+    
+
+    //视图挂载到页面上了, 这里可操作DOM
+    mounted() {
+        $(function() {
+
+            var magnifierConfig = {
+                magnifier: "#magnifier1",//最外层的大容器
+                width: 380,//承载容器宽
+                height: 380,//承载容器高
+                moveWidth: null,//如果设置了移动盒子的宽度，则不计算缩放比例
+                zoom: 3//缩放比例
+            };
+
+            //因为我们使用v-for动态生成列表，调用该方法时不能保证列表已经构建完毕, 所以延时一下
+            //调用这个插件，必须保证放大镜相关的DOM节点都已正常构建渲染
+            setTimeout(function() {
+                var _magnifier = $().imgzoon(magnifierConfig);
+            }, 500);
+
+
+
+        })
+    },
+
+
+
+}
 </script>
 
 <style scoped>
